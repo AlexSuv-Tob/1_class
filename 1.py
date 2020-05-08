9e02294aaf08440920ad444df3acdb311298b749
AlexSuv-Tob <58843609+AlexSuv-Tob@users.noreply.github.com>
2020-05-08 12:33:37 +0500

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,14 @@
+name = input('введите имя: ')
+print(name)
+
+surname = input('Введите Фамилию: ')
+print(surname)
+
+auto = input('Какой марки Ваш автомобиль: ')
+print(auto)
+
+age = int(input('Введите Ваш возраст: '))
+print(age)
+
+year = int(input('Введите год вашего рождения: '))
+print(year)
